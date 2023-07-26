f5a533786b1efb7625e02db629662d9865ec0fc0
Adi <69413640+adisagar2003@users.noreply.github.com>
2023-07-26 04:16:39 -0230

    Update Api.ts

--- src/lib/api/Api.ts
+++ src/lib/api/Api.ts
@@ -1,2 +1,2 @@
-export const API_URL: string = "https://chatapp-b6xk.onrender.com/";
+export const API_URL: string = "http://18.224.71.223:3300/";
 // export const API_URL:string = "http://localhost:3300/";
